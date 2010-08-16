64ab5e2e68bdf0a12a382c3ddf4ad8bb60e634a2
Grzegorz <grzegorz@enzos-computer.local>
2010-08-16 17:26:48 +0200

    code style fixes

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,86 +1,82 @@
 require 'rubygems'
-require 'colorize'    
+require 'colorize'
 require 'fileutils'
-require 'ruby-debug'    
+require 'ruby-debug'
 require 'chronic'
 require 'spec'
-require 'test/unit/assertions'         
+require 'test/unit/assertions'
 require 'features/support/create_screenshot_folder'
 require 'features/support/screenshot'
 require 'cucumber/formatter/unicode'
-include Test::Unit::Assertions 
+include Test::Unit::Assertions
 
 #set browser : ie / firefox / safari / chrome / celerity
 BROWSER = "firefox"
 
 #set a fixture file with translation separate with space "test_de test_en"
-LANGUAGE = %w/sanity_check_de/
-                 
+LANGUAGE = %w/sanity_check_pl/
+
 case BROWSER
-  when "safari"
-    require 'safariwatir'
-    Browser = Watir::Safari.new
- 
-  when "firefox"
+when "safari"
+  require 'safariwatir'
+  Browser = Watir::Safari.new
 
-    require 'firewatir'
-    Browser = FireWatir::Firefox.new  
+when "firefox"
 
- #  require 'watir-webdriver'
- #  Browser = Watir::Browser.new :firefox
-     # require 'vapir'
-     # require 'vapir-firefox'
-     #  Browser = Vapir::Firefox.new
-  
-  when "chrome"
-    require 'watir-webdriver'
-    Browser = Watir::Browser.new :chrome 
-     
- when "ie"
-    require 'watir' 
-    require 'watir/ie'
-    require 'win32ole'    
-    require 'win32/process'
-    Browser = Watir::IE.new
-    Browser.speed = :zippy #browser speed :fast / :zippy
+  require 'firewatir'
+  Browser = FireWatir::Firefox.new
 
- when "celerity"
-   require 'celerity'
-   Browser = Celerity::Browser.new
+  #   require 'watir-webdriver'
+  #  Browser = Watir::Browser.new :firefox
+  #      require 'vapir'
+  #     require 'vapir-firefox'
+  #     Browser = Vapir::Firefox.new
+
+when "chrome"
+  require 'watir-webdriver'
+  Browser = Watir::Browser.new :chrome
+
+when "ie"
+  require 'watir'
+  require 'watir/ie'
+  require 'win32ole'
+  require 'win32/process'
+  Browser = Watir::IE.new
+  Browser.speed = :zippy #browser speed :fast / :zippy
+
+when "celerity"
+  require 'celerity'
+  Browser = Celerity::Browser.new
 else
-    raise "This platform is not supported (#{PLATFORM})"
+  raise "This platform is not supported (#{PLATFORM})"
 end
-                                      
-                        
+
+
 #create screenshot folder function
 screenshot_path = create_screenshot_folder
-                                             
- browser = Browser
- # "before all"
- Before do    
-   @table = {}
-   
-   @screenshot_path = screenshot_path
 
-   @fixtures = LANGUAGE
-   @fixtures.each { |table| @table.merge! YAML.load_file("features/fixtures/#{table}.yml") } 
-   
-   @browser = browser
-   @environment = "http://"
- end
-         
+browser = Browser
+# "before all"
+Before do
+  @table = {}
+
+  @screenshot_path = screenshot_path
+
+  @fixtures = LANGUAGE
+  @fixtures.each { |table| @table.merge! YAML.load_file("features/fixtures/#{table}.yml") }
+
+  @browser = browser
+  @environment = "http://"
+end
+
 # after each step which is called '@new_feature' make a screenshot
 
-AfterStep('@new_feature') do    
-  embed_screenshot("#{@screenshot_path}screenshot-#{Time.new.to_i}") 
+AfterStep('@new_feature') do
+  embed_screenshot("#{@screenshot_path}screenshot-#{Time.new.to_i}")
 end
- 
- # "after all"
- After do           
-   # can close the browser after test
-    # @browser.close 
- end
 
-
- 
-  
+# "after all"
+After do
+  # can close the browser after test
+  # @browser.close
+end
