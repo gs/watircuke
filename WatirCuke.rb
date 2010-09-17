8d63d16e0c34324e03d7ff849dabc022171fdc6b
Grzegorz <grzegorz.smajdor@gmail.com>
2010-09-17 15:23:56 +0200

    small modifications

--- WatirCuke.rb
+++ WatirCuke.rb
@@ -3,10 +3,10 @@ require 'rubygems'
 require 'cucumber'
 require 'colorize'
 
-
 require 'features/support/create_screenshot_folder'
-#this are the tests that have config_ files in config/machine/config..yml files - designed to work with sanity checks
-tests=%w/sc_bbc_en sc_bbc_de sc_bbc_pl sc_bbc_es sc_staging_en sc_staging_de sc_staging_pl sc_staging_es sc_live_en sc_live_de sc_live_pl sc_live_es/
+
+#this are the tests that have config_ files in config/machine/config_*.yml files - designed to work with sanity checks
+tests=%w/bbc_en bbc_de bbc_pl bbc_es staging_en staging_de staging_pl staging_es live_en live_de live_pl live_es/
 
 class RunMe                       
   
@@ -20,7 +20,7 @@ class RunMe
       test_name = name_the_test
       copy_config_yml_file
       folder = setup(ARGV[0])
-      system "cucumber --guess -t @#{test_name} -f html > #{folder}/../#{test_name}.html"
+      system "cucumber --guess -t @#{test_name} -f html > #{folder}/../#{ARGV[0]}.html"
     elsif ((("#{ARGV[0]}" =~/\@/) || ("#{ARGV[0]}" =~ /features/)) && (!"#{ARGV[1]}".nil?))
       folder = setup(ARGV[1] ||= ARGV[0])
       system "cucumber --guess -t #{ARGV[0]} -f html > #{folder}/../#{ARGV[1]}.html"
@@ -36,7 +36,7 @@ class RunMe
       print "or with any of the tests: "
       print "#{@tests}".yellow
       puts
-      puts "./WatirCuke.rb sc_live_en".green
+      puts "./WatirCuke.rb live_en".green
     end
   end
 
@@ -46,11 +46,11 @@ class RunMe
   end
 
   def name_the_test
-    ARGV[0].gsub(/\_\w+\_/,"_")
+    ARGV[0].gsub(/\w+\_/,"")
   end
 
   def copy_config_yml_file
-    config_name = ARGV[0].gsub("sc", "config")
+    config_name = "config_" + ARGV[0].to_s
     FileUtils.cp "./config/machines/#{config_name}.yml", "./config/config.yml"
   end
 
