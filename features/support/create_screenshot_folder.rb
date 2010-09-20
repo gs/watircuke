3c9082de741f8d3f43230dcf26524045b49bf79f
Grzegorz <grzegorz.smajdor@gmail.com>
2010-09-20 13:07:37 +0200

    README update, no particular changes

--- features/support/create_screenshot_folder.rb
+++ features/support/create_screenshot_folder.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 def create_screenshot_folder(folder, t)     
   begin
     FileUtils.mkpath("test_results/#{folder}_#{t}/screenshots")
-  rescue Exception => ex
+  rescue Exception => ex                                      
     puts "#{ex}".red
   end
  end
